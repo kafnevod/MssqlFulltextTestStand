6e9ecdc8e5fa73338afbbeb137e015fb45275e31
Flexberry-man <mail@flexberry.net>
2021-02-20 05:06:49 +0000

    Flexberry code generation

--- src/ember-app/app/services/store.js
+++ src/ember-app/app/services/store.js
@@ -1,22 +1,3 @@
-import BaseStore from 'ember-flexberry-data/stores/base-store';
 import StoreMixin from 'ember-flexberry-data/mixins/store';
-import config from '../config/environment';
-import { inject as service } from '@ember/service';
-
-export default BaseStore.reopen(StoreMixin, {
-  /**
-   Service that return offline schemas.
-
-   @property objectlistviewEventsService
-   @type {Class}
-   @default OfflineGlobalsService
-  */
-  offlineGlobals: service('offline-globals'),
-
-  init() {
-    this.set('offlineSchema', {
-      [config.APP.offline.dbName]: { 1: this.get('offlineGlobals').getOfflineSchema() },
-    });
-    return this._super(...arguments);
-  }
-});
+import OnlineStore from 'ember-flexberry-data/stores/online-store';
+export default OnlineStore.reopen(StoreMixin);
